e489785dd0a5671442abdac60fe0591d4a8108e5
Kade <KadePcGames@gmail.com>
2020-01-18 20:11:50 -0800

    Intial Commit.

---
+++ ChopperDrop/EventHandlers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using MEC;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using EXILED;
+
+namespace ChopperDrop
+{
+    public class EventHandlers
+    {
+        public Plugin pl;
+        public ChopperDrops allowedItems;
+
+        public List<CoroutineHandle> coroutines = new List<CoroutineHandle>();
+
+        public bool roundStarted = false;
+
+        public EventHandlers(Plugin plugin, ChopperDrops drops) 
+        { 
+            pl = plugin;
+            allowedItems = drops;
+        }
+
+        internal void RoundStart()
+        {
+            roundStarted = true;
+            foreach (CoroutineHandle handle in coroutines)
+                Timing.KillCoroutines(handle);
+            Plugin.Info("Starting the ChopperThread.");
+            coroutines.Add(Timing.RunCoroutine(ChopperThread(), "ChopperThread"));
+        }
+
+        internal void WaitingForPlayers()
+        {
+            foreach (CoroutineHandle handle in coroutines)
+                Timing.KillCoroutines(handle);
+        }
+
+        public IEnumerator<float> ChopperThread()
+        {
+            while(roundStarted)
+            {
+                // Unity GARBAGE
+                Plugin.Info("Chopper thread waiting 10 minutes.");
+                yield return Timing.WaitForSeconds(600); // Wait 600 seconds (10 minutes)
+                Plugin.Info("Spawning chopper!");
+                ChopperAutostart ca = UnityEngine.Object.FindObjectOfType<ChopperAutostart>(); // Call in the chopper
+                ca.SetState(true);
+
+                yield return Timing.WaitForSeconds(15); // Wait 15 seconds
+
+                Vector3 spawn = Plugin.GetRandomSpawnPoint(RoleType.NtfCadet); // Get the spawn point of the chopper to you know, spawn em.
+
+                foreach (KeyValuePair<ItemType, int> drop in allowedItems.drops) // Drop items
+                {
+                    Plugin.Info("Spawning " + drop.Value + " " + drop.Key.ToString() + "'s");
+                    for (int i = 0; i < drop.Value; i++)
+                    {
+                        SpawnItem(drop.Key, spawn, spawn);
+                    }
+                }
+                ca.SetState(false); // Call the chopper to leave
+                yield return Timing.WaitForSeconds(15); // Wait 15 seconds to let the chopper leave.
+            }
+        }
+
+        public void SpawnItem(ItemType type, Vector3 pos, Vector3 rot)
+        {
+            PlayerManager.localPlayer.GetComponent<Inventory>().SetPickup(type, -4.656647E+11f, pos, Quaternion.Euler(rot), 0, 0, 0);
+        }
+    }
+}
---
+++ ChopperDrop/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using EXILED;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChopperDrop
+{
+    public class ChopperDrop : EXILED.Plugin
+    {
+        public EventHandlers EventHandlers;
+
+        public override string getName => "ChopperDrop";
+
+        public override void OnDisable()
+        {
+            foreach (CoroutineHandle handle in EventHandlers.coroutines)
+                Timing.KillCoroutines(handle);
+
+            Events.RoundStartEvent -= EventHandlers.RoundStart;
+            Events.WaitingForPlayersEvent -= EventHandlers.WaitingForPlayers;
+
+            EventHandlers = null;
+        }
+
+        public override void OnEnable()
+        {
+            // We make our own dictionary stuff because the .GetStringDictionary of 'config' me and joker don't know how it works lol.
+            string[] drops = Config.GetString("chopper_drops", "GrenadeFrag:4,Flashlight:1,GunMP7:4,GunUSP:2,Painkillers:4").Split(',');
+            ChopperDrops cDrops = new ChopperDrops();
+            
+            foreach (string drop in drops)
+            {
+                string[] d = drop.Split(':'); // d[0] = item, d[1] = amount
+                cDrops.AddToList(d[0], int.Parse(d[1]));
+            }
+
+            EventHandlers = new EventHandlers(this, cDrops);
+            Events.RoundStartEvent += EventHandlers.RoundStart;
+            Events.WaitingForPlayersEvent += EventHandlers.WaitingForPlayers;
+
+            Info("Chopper Drop enabled! Enjoy :D");
+        }
+
+        public override void OnReload()
+        {
+            // empty
+        }
+    }
+}
